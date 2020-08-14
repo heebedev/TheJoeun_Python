9aaae96df637b22050d55b1d433a3b6260215230
zzeung615 <zzeung615@gmail.com>
2020-08-14 18:25:54 +0900

    0814

---
+++ Note/mod1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum(x,y) :
+    return x+y
+
---
+++ Note/mod2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum(a,b):
+    if type(a) != type(b) :
+        print("변수의 타입이 다릅니다.")
+    else : return a+b
+
